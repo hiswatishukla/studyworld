b60ae684c98b92473cfca71144eb602a6dc878bd
Atul Kumar Nag <atulnag12@gmail.com>
2024-09-18 22:09:16 +0530

    Adding Project to the github

---
+++ studyworld/src/main/java/com/tutorials/studyworld/StudyworldApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.studyworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyworldApplication.class, args);
+	}
+
+}
---
+++ studyworld/src/test/java/com/tutorials/studyworld/StudyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.studyworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
